40ce2812b97f4d378fb6222e1ef291f172e9b715
diok22 <Dionysis.k22@gmail.com>
2018-07-06 01:24:38 +0300

    completes DotCom class with comments

--- ChapterSix/DotCom.java
+++ ChapterSix/DotCom.java
@@ -1,23 +1,29 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Dot Com {
+  // instance variables
+  private ArrayList<String> locationCells;
+  private String name;
 
-  public ArrayList<String> locationCells;
-
+  // setter method for the DotCom's locations
   public void setLocationCells(ArrayList<String> loc) {
     locationCells = loc;
   }
 
+  // basic setter method
+  public void setName(String n) {
+    name = n;
+  }
+
   public String checkYourself(String userInput) {
     String result = "miss";
-
     int index = locationCells.indexOf(userInput);
-
     if (index >= 0) {
-      locationCells.remove(index);
+      locationCells.remove(index); // delete entry
 
-      if (locationCells.isEmpty()) {
+      if (locationCells.isEmpty()) { // if location has been guessed
         result = "kill";
+        System.out.println("Ouch! You sunk " + name + "  : ( ");
       } else {
         result = "hit";
       }
